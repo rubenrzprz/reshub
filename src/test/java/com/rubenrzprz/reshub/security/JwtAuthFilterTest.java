abc1fdac5544bf5ff56514d2cee36c422ab5eb9e
Rubén Ruiz Pérez <rubenrzprz@gmail.com>
2026-03-04 21:12:21 +0000

    Revert "test(security): remove Mockito dependency from JwtAuthFilterTest"
    This reverts commit 7757f2917892dc25dc53308b6fba1bc0cbe02f42.

--- src/test/java/com/rubenrzprz/reshub/security/JwtAuthFilterTest.java
+++ src/test/java/com/rubenrzprz/reshub/security/JwtAuthFilterTest.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
+import static org.mockito.Mockito.mock;
+
 class JwtAuthFilterTest {
 
-  private static final String TEST_JWT_SECRET = "test-secret-key-with-at-least-32-characters";
   private final JwtAuthFilter filter = new JwtAuthFilter(
-    new JwtService(new JwtProperties(TEST_JWT_SECRET, "reshub-test", 60)),
-    (request, response, handler, ex) -> null
+    mock(JwtService.class),
+    mock(HandlerExceptionResolver.class)
   );
 
   @Test
